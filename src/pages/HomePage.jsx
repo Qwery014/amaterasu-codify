fae972943a49e370a7c956d218527c7cf01a8351
sharshenalievbarni@gmail.com <sharshenalievbarni@gmail.com>
2023-03-11 17:39:25 +0600

    navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
+import { Button } from '@mui/material';
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar'
+import "../styles/Homepage.css"
+
 
 const HomePage = () => {
+  const navigate = useNavigate()
   return (
-    <div>
-      
-    </div>
+   
+    <div className='HomePage'>
+ <div className='HomeInfo'><p>Здравствуйте</p>
+ <p>Добро пожаловать в наш сайт</p>
+ <Button variant="contained" onClick={()=>navigate('profile')}>Наш каталог</Button>
+ 
+ </div>
+
+
+        </div>
   );
 };
 
